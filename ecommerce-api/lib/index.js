e0045ff8b55ee1a9c73a18ea9b1ea08b90e8ce7a
Vânio Anibal  Macamo <macamovanioanibal@gmail.com>
2025-04-29 17:12:53 +0200

    Express e os metodos HTTP

--- ecommerce-api/lib/index.js
+++ ecommerce-api/lib/index.js
@@ -5,21 +5,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 app.get("/", (req, res) => {
     res.send("Hello World com tsc watch!");
 });
+let users = [
+    {
+        name: "John",
+        age: 30,
+    },
+    {
+        name: "Jane",
+        age: 25,
+    },
+];
 app.get("/users", (req, res) => {
-    let users = [
-        {
-            name: "John",
-            age: 30,
-        },
-        {
-            name: "Jane",
-            age: 25,
-        },
-    ];
     res.send(users);
+});
+app.post("/users", (req, res) => {
+    let user = req.body;
+    users.push(user);
+    res.send({
+        "message": "User created successfully"
+    });
 });
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
--- ecommerce-api/src/index.ts
+++ ecommerce-api/src/index.ts
@@ -2,23 +2,35 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World com tsc watch!");
 });
 
-app.get("/users", (req: Request, res: Response) => {
-  let users = [
-    {
-      name: "John",
-      age: 30,
-    },
-    {
-      name: "Jane",
-      age: 25,
-    },
-  ];
+let users = [
+  {
+    name: "John",
+    age: 30,
+  },
+  {
+    name: "Jane",
+    age: 25,
+  },
+];
 
+app.get("/users", (req: Request, res: Response) => {
   res.send(users);
+});
+
+app.post("/users", (req: Request, res: Response) => {
+  let user = req.body;
+
+  users.push(user);
+
+  res.send({
+    "message": "User created successfully"
+  });
 });
 
 app.listen(3000, () => {
