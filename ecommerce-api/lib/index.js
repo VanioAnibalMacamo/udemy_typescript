0bd6751df6649c376b1165db1a30b6bb2c81a858
Vânio Anibal  Macamo <macamovanioanibal@gmail.com>
2025-04-29 17:01:56 +0200

    #Improvment: Inicio aos metodos REST

--- ecommerce-api/lib/index.js
+++ ecommerce-api/lib/index.js
@@ -6,7 +6,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World com tsc watch!");
+});
+app.get("/users", (req, res) => {
+    let users = [
+        {
+            name: "John",
+            age: 30,
+        },
+        {
+            name: "Jane",
+            age: 25,
+        },
+    ];
+    res.send(users);
 });
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
--- ecommerce-api/src/index.ts
+++ ecommerce-api/src/index.ts
@@ -1,11 +1,25 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/", (req , res) =>{
-    res.send("Hello World!");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World com tsc watch!");
 });
 
+app.get("/users", (req: Request, res: Response) => {
+  let users = [
+    {
+      name: "John",
+      age: 30,
+    },
+    {
+      name: "Jane",
+      age: 25,
+    },
+  ];
+
+  res.send(users);
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
