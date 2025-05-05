1da0a0b4ad9e649e62b4d4885e7c89cd8c5c3c94
Vânio Anibal  Macamo <macamovanioanibal@gmail.com>
2025-05-05 11:57:58 +0200

    #Funcao delete e put

--- ecommerce-api/lib/index.js
+++ ecommerce-api/lib/index.js
@@ -9,25 +9,55 @@ app.use(express_1.default.json());
 app.get("/", (req, res) => {
     res.send("Hello World com tsc watch!");
 });
-let users = [
-    {
-        name: "John",
-        age: 30,
-    },
-    {
-        name: "Jane",
-        age: 25,
-    },
-];
+let id = 0;
+let usuarios = [];
 app.get("/users", (req, res) => {
-    res.send(users);
+    res.send(usuarios);
 });
 app.post("/users", (req, res) => {
-    let user = req.body;
-    users.push(user);
+    let usuario = req.body;
+    usuario.id = ++id;
+    usuarios.push(usuario);
     res.send({
-        "message": "User created successfully"
+        message: "User created successfully",
     });
+});
+app.get("/users/:id", (req, res) => {
+    let id = Number(req.params.id);
+    let usuario = usuarios.find((usuario) => usuario.id === id);
+    if (usuario) {
+        res.send(usuario);
+    }
+    else {
+        res.send({
+            message: "User not found",
+        });
+    }
+});
+app.put("/users/:id", (req, res) => {
+    let id = Number(req.params.id);
+    let { name, email } = req.body;
+    let indexOf = usuarios.findIndex((_user) => _user.id === id);
+    usuarios[indexOf].name = name;
+    usuarios[indexOf].email = email;
+    res.send({
+        message: "Usuario atualizado com sucesso!",
+    });
+});
+app.delete("/users/:id", (req, res) => {
+    let id = Number(req.params.id);
+    let usuario = usuarios.find((usuario) => usuario.id === id);
+    if (usuario) {
+        usuarios = usuarios.filter((usuario) => usuario.id !== id);
+        res.send({
+            message: "Usuario Removido",
+        });
+    }
+    else {
+        res.send({
+            message: "User deleted successfully",
+        });
+    }
 });
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
