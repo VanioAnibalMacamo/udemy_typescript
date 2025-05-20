ffe6dde3b0dc0e9e1b40daa5868d7371c4972def
Vânio Anibal  Macamo <macamovanioanibal@gmail.com>
2025-05-20 16:31:49 +0200

    Remover lib, .env e firebase-adminsdk.json do versionamento; manter código atualizado

---
+++ ecommerce-api/lib/controllers/users.controller.js
@@ -0,0 +1,71 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UsersController = void 0;
+const firestore_1 = require("firebase-admin/firestore");
+let usuarios = [];
+class UsersController {
+    static getAll(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const snapshot = yield (0, firestore_1.getFirestore)().collection('users').get();
+            const users = snapshot.docs.map((doc) => {
+                return Object.assign({ id: doc.id }, doc.data());
+            });
+            res.send(users);
+        });
+    }
+    static getById(req, res) {
+        const { id } = req.params;
+        const usuario = usuarios.find((user) => user.id === Number(id));
+        if (usuario) {
+            res.send(usuario);
+        }
+        else {
+            res.status(404).send("Usuário não encontrado");
+        }
+    }
+    static save(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let user = req.body;
+            const userSalvo = yield (0, firestore_1.getFirestore)().collection('users').add(user);
+            res.send({
+                message: `User created ${userSalvo.id} successfully`,
+            });
+        });
+    }
+    static update(req, res) {
+        let id = Number(req.params.id);
+        let { name, email } = req.body;
+        let indexOf = usuarios.findIndex((_user) => _user.id === id);
+        usuarios[indexOf].name = name;
+        usuarios[indexOf].email = email;
+        res.send({
+            message: "Usuario atualizado com sucesso!",
+        });
+    }
+    static delete(req, res) {
+        let id = Number(req.params.id);
+        let usuario = usuarios.find((usuario) => usuario.id === id);
+        if (usuario) {
+            usuarios = usuarios.filter((usuario) => usuario.id !== id);
+            res.send({
+                message: "Usuario Removido",
+            });
+        }
+        else {
+            res.send({
+                message: "User deleted successfully",
+            });
+        }
+    }
+}
+exports.UsersController = UsersController;
+//# sourceMappingURL=users.controller.js.map
---
+++ ecommerce-api/lib/index.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app_1 = require("firebase-admin/app");
+const index_1 = require("./routes/index");
+(0, app_1.initializeApp)();
+const app = (0, express_1.default)();
+(0, index_1.routes)(app);
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+//# sourceMappingURL=index.js.map
